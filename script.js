d465914b3219b60354f3e5eeabe4ef03dcf5f016
aharral09 <aharral09@gmail.com>
2022-01-16 00:45:55 -0500

    init commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// my first comment
+console.log('hello world!');
+
+
+// declare variable
+let myName = 'Alex Harral';
+console.log(myName);
+
+// declare constant variable
+const rate = 0.3;
+//rate = 1;
+console.log(rate)
+
